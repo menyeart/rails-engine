4a727f14362ae2db9dce1264242dd761c5774240
Matt Enyeart <menyeart1@gmail.com>
2023-03-27 17:13:56 -0600

    Add show action to merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,14 @@
+
 module Api
   module V1
     class MerchantsController < ApplicationController
 
       def index
         render json: MerchantSerializer.new(Merchant.all)
+      end
+
+      def show
+        render json: MerchantSerializer.new(Merchant.find(params[:id]))
       end
     end
   end
