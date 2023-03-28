afc354eef712529321fa4521426cd227f56f0700
Matt Enyeart <menyeart1@gmail.com>
2023-03-28 13:23:54 -0600

    Add create action to items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,6 +8,21 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: ItemSerializer.new(item), status: 201
+    else
+      render_not_created_response
+    end
+  end
+   
 
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id )
+  end
 
 end
