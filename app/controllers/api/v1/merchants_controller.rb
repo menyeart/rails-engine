6e28139c2fadb7dd75d858fac5e943992c3853ee
Matt Enyeart <menyeart1@gmail.com>
2023-03-27 20:17:14 -0600

    Change Merchants controller to shorter naming syntax

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,15 +1,10 @@
+class Api::V1::MerchantsController < ApplicationController
 
-module Api
-  module V1
-    class MerchantsController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.all)
+   end
 
-      def index
-        render json: MerchantSerializer.new(Merchant.all)
-      end
-
-      def show
-        render json: MerchantSerializer.new(Merchant.find(params[:id]))
-      end
-    end
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
