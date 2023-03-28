d7d2e7d973866e7c89028fd2e735e1ef05901571
Matt Enyeart <menyeart1@gmail.com>
2023-03-28 10:44:14 -0600

    Create items controller show action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::ItemsController < ApplicationController
 
   def index
     render json: ItemSerializer.new(Item.all)
-   end
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
 
 
 end
