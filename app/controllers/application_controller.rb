f1cbe4b9dc3fb78e0aa91622e87302c3c583c021
Matt Enyeart <menyeart1@gmail.com>
2023-03-28 10:26:06 -0600

    Add code to application controller to set up error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::API
   def render_not_found_response(exception)
     render json: { message: "your query could not be completed", error: exception.message }, status: :not_found
   end
+
+  # def render_not_found_response
+  #   render json: {
+  #     "id": nil,
+  #     "name": nil,
+  #     "created_at": nil,
+  #     "updated_at": nil
+  #   }
+  # end
 end
