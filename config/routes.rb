0cc367f7d18c72b74305b3d0b12a2f798da3f7a2
Matt Enyeart <menyeart1@gmail.com>
2023-03-29 14:43:37 -0600

    Change item find route to item find all

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get "/api/v1/merchants/find", to: "api/v1/merchants#find"
+  get "/api/v1/items/find_all", to: "api/v1/items#find_all"
+
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
