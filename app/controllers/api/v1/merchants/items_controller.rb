7fc0373fba5a8752deee235e1635eb67139b649e
Matt Enyeart <menyeart1@gmail.com>
2023-03-29 17:29:02 -0600

    Remove extra spacing

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
-      def index
-        merchant = Merchant.find(params[:merchant_id])
-        render json: ItemSerializer.new(merchant.items)
-      end
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    render json: ItemSerializer.new(merchant.items)
+  end
 end
