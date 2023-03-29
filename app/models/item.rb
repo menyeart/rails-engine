7f87d9cd7a063231968d1578dc48b6e6160d9907
Matt Enyeart <menyeart1@gmail.com>
2023-03-29 17:30:20 -0600

    Add invoices only item method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items, :dependent => :destroy
   belongs_to :merchant
+  before_destroy :invoices_only_item
 
   validates_presence_of :name,
                         :description,
@@ -22,6 +25,13 @@ class Item < ApplicationRecord
     Item.where("items.unit_price < ?", max)
   end
 
+  def invoices_only_item
+    self.invoices.having("count(invoice_items.id) = 1").group("invoices.id")
+  end
 
+  private
 
+  # def cleanup
+  #   self.invoice_items.destroy_all &&  self.invoices.destroy_all if self.invoices.count == 1
+  # end
 end
