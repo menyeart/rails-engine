33a708568d1d9b1200ee7f6b6c8ef114aad17eea
Matt Enyeart <menyeart1@gmail.com>
2023-03-30 16:04:53 -0600

    Add tests for merchant model validations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
   end
 
+  describe "validations" do
+    it { should validate_presence_of :name }
+  
+  end
+
   describe "methods" do
     it "can search for merchants by name, order them by name ascending and return the first one" do
       matty = create(:merchant, name: "Matty")
