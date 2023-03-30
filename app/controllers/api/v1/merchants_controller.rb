cadf75bcfa2fd3d490e42b90e37210c3bb4d5381
Matt Enyeart <menyeart1@gmail.com>
2023-03-30 11:28:51 -0600

    Remove find all method from merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -7,9 +7,4 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
-
-  def find
-    merchant = Merchant.find_by_name(params[:name].downcase)
-    render json: MerchantSerializer.new(merchant)
-  end
 end
