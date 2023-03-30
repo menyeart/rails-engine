7532abd224e73b0f1869dc752dd08c0832045717
Matt Enyeart <menyeart1@gmail.com>
2023-03-30 15:59:47 -0600

    Refactor find controller to show action to use serializer

--- app/controllers/api/v1/find_controller.rb
+++ app/controllers/api/v1/find_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::FindController < ApplicationController
   def show
-    merchant = Merchant.find_by_name(params[:name].downcase)
-    render json: MerchantSerializer.new(merchant)
+    begin
+      merchant = Merchant.find_by_name(params[:name].downcase)
+      render json: MerchantSerializer.new(merchant)
+    rescue => exception
+      render json: ErrorSerializer.error_to_empty, status: 200
+    end  
   end
 end
