2bf3f9bd330dd43e7b34df8573660796de94b2ff
Matt Enyeart <menyeart1@gmail.com>
2023-03-30 16:00:50 -0600

    Remove unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :render_error
 
-  def render_not_found_response(exception)
-    render json: { message: "your query could not be completed", error: exception.message }, status: :not_found
-  end
-
   def render_not_created_response
     render json: { message: "your query could not be completed", error: "Object creation could not be completed" }, status: 400
   end
